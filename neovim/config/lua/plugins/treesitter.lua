e40688feb5737fadda725603a9135ac970729f9b
DiSkills <github@diskills-developer.org>
2026-01-09 11:53:00 -0500

    neovim: fixed treesitter

--- neovim/config/lua/plugins/treesitter.lua
+++ neovim/config/lua/plugins/treesitter.lua
@@ -1,11 +1,18 @@
-require('nvim-treesitter').install({
+local parsers = {
   "bash", "zsh",
-  "c", "cpp", "cmake", "nasm", "rust",
+  "c", "cpp", "make", "cmake", "nasm", "rust",
   "python", "javascript",
   "dockerfile", "gitignore", "nginx",
   "csv", "http", "json", "xml", "sql",
   "latex", "markdown",
   "lua", "vim", "vimdoc",
-})
+}
 
-vim.treesitter.start()
+require('nvim-treesitter').install(parsers)
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = parsers,
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
